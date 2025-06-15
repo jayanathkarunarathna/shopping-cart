e36c75dd681040319ba9c3b56650169171cc6c20
Harshana Perera <whmperera@gmail.com>
2025-06-16 03:21:41 +1200

    Update BaseClass

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -23,13 +24,28 @@ public class BaseClass {
         //Loading log4j file
         logger = LogManager.getLogger(this.getClass());
 
-        //Open Chrome Browser
-        driver = new ChromeDriver();
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        ChromeOptions options = new ChromeOptions();
 
-        driver.get("http://jupiter.cloud.planittesting.com");
-        driver.manage().window().maximize();
+        String githubActions = System.getenv("GITHUB_ACTIONS");
+
+        if ("true".equalsIgnoreCase(githubActions)) {
+            // Running in GitHub Actions - run headless
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+
+            driver = new ChromeDriver(options);
+        }
+        else {
+            //Open Chrome Browser
+            driver = new ChromeDriver();
+            driver.manage().deleteAllCookies();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            driver.get("http://jupiter.cloud.planittesting.com");
+            driver.manage().window().maximize();
+        }
 
     }
 
