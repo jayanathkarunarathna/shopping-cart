8be3f036922e2f00901e13bf283b1caabf3db6d6
jayanath <jayanathkarunarathna@gmail.com>
2025-06-16 13:18:23 +0800

    moved extent reports into target

--- src/test/java/utilities/ExtentReportManager.java
+++ src/test/java/utilities/ExtentReportManager.java
@@ -29,7 +29,8 @@ public class ExtentReportManager implements ITestListener {
 
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());// time stamp
         repName = "Test-Report-" + timeStamp + ".html";
-        sparkReporter = new ExtentSparkReporter(".\\reports\\" + repName);// specify location of the report
+        String path = System.getProperty("user.dir")+"/target/extent/" + repName;
+        sparkReporter = new ExtentSparkReporter(path);// specify location of the report
 
         sparkReporter.config().setDocumentTitle("Automation Report"); // TiTle of report
         sparkReporter.config().setReportName("Functional Testing"); // name of the report
